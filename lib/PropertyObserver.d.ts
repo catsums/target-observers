c6af54ba96d43f76fd8394b874d377e4832660ae
Cassim <111745304+catsums@users.noreply.github.com>
2023-11-16 16:43:13 +0200

    Added main lib

---
+++ lib/PropertyObserver.d.ts
@@ -0,0 +1,26 @@
+import { ProcessingTarget } from './ProcessingTarget';
+export declare class PropertyObserver extends ProcessingTarget {
+    targetName: string;
+    _precision: number;
+    _observedElements: Object;
+    _proxies: Map<any, any>;
+    _entriesQueue: any[];
+    _callback: (entries: any, obs: any) => void;
+    constructor(callback: (ent: any[], obs: ProcessingTarget) => void, { FPS, frameTolerance, active, precision }: {
+        FPS?: number;
+        frameTolerance?: number;
+        active?: boolean;
+        precision?: number;
+    });
+    onPhysicsProcess(delta: any): void;
+    checkUpdates(elements: Object[]): Promise<void>;
+    checkUpdate(elem: Object): Promise<{
+        id: any;
+        target: Object;
+        changes: {};
+    }>;
+    handleEntries(ents: any): void;
+    processEntry(elem: any, entry: any, process: any): void;
+    observe(elem: Object | Object[], opts: Object | any[]): any[];
+    disconnect(elem: Object): void;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,15 @@
+export { ProcessingTarget } from "./ProcessingTarget";
+export { TransformObserver } from "./TransformObserver";
+export * as ElementFunctions from "./ElementFunctions";
+export { CollisionObserver } from "./CollisionObserver";
+import { ProcessingTarget } from "./ProcessingTarget";
+import { TransformObserver } from "./TransformObserver";
+import * as ElementFunctions from "./ElementFunctions";
+import { CollisionObserver } from "./CollisionObserver";
+declare const _default: {
+    ProcessingTarget: typeof ProcessingTarget;
+    TransformObserver: typeof TransformObserver;
+    ElementFunctions: typeof ElementFunctions;
+    CollisionObserver: typeof CollisionObserver;
+};
+export default _default;
